0b0c6e52ccb70f7453f1084584d3a1a52d71f3d9
QeeeXXX <zakharchenkoserg297@gmail.com>
2024-11-21 13:52:27 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -9,3 +9,7 @@ function getArrayParams(...arr) {
 
     return { max, min, avg };
 }
+// Примеры вызова функций//
+console.log(getArrayParams(-99, 99, 10));   // { min: -99, max: 99, avg: 3.33 }
+console.log(getArrayParams(1, 2, 3, -100, 10));  // { min: -100, max: 10, avg: -16.8 }
+console.log(getArrayParams(5));  // { min: 5, max: 5, avg: 5 }
