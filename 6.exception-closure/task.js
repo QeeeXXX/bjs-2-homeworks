f44e5a4cf37264e39cb79dfe54cb15a3b30716b7
QeeeXXX <zakharchenkoserg297@gmail.com>
2024-12-12 13:01:11 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,5 +1,6 @@
 // Задача 1 //
 // Функция для парсинга числа
+// Определение функций
 function parseCount(value) {
   const result = Number.parseFloat(value);
 
@@ -10,7 +11,6 @@ function parseCount(value) {
   return result;
 }
 
-// Функция для проверки значения
 function validateCount(value) {
   try {
     return parseCount(value);
@@ -19,3 +19,28 @@ function validateCount(value) {
     return error;
   }
 }
+
+// Тесты
+describe('Домашнее задание к лекции «Обработка исключений и замыкания»', () => {
+  describe('Задача №1', () => {
+    it('функция parseCount должна парсить целое значение', () => {
+      expect(parseCount("123")).toEqual(123);
+    });
+
+    it('функция parseCount должна парсить значение 012', () => {
+      expect(parseCount("012")).toEqual(12);
+    });
+
+    it('функция validateCount должна парсить дробное значение', () => {
+      expect(validateCount("56.65")).toEqual(56.65);
+    });
+
+    it('функция parseCount не должна парсить невалидное значение', () => {
+      expect(() => parseCount("ыфва")).toThrowError("Невалидное значение");
+    });
+
+    it('функция validateCount должна возвращать перехваченную ошибку', () => {
+      expect(validateCount("ыфва").stack.includes("parseCount")).toBeTruthy();
+    });
+  });
+});
