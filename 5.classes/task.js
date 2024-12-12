3d16b68fbefa592141e131abe04802a56e4ac482
QeeeXXX <zakharchenkoserg297@gmail.com>
2024-12-12 11:44:12 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -79,72 +79,6 @@ console.log(detectiveBook.state); // 90
 
 // Задача 2 //
 
-// Базовые классы для печатных изданий
-class PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    this.name = name;
-    this.releaseDate = releaseDate;
-    this.pagesCount = pagesCount;
-    this._state = 100; // начальное состояние издания
-    this.type = null;
-  }
-
-  get state() {
-    return this._state;
-  }
-
-  set state(newState) {
-    if (newState < 0) {
-      this._state = 0;
-    } else if (newState > 100) {
-      this._state = 100;
-    } else {
-      this._state = newState;
-    }
-  }
-
-  fix() {
-    this._state = Math.min(this._state * 1.5, 100); // увеличение состояния с учётом ограничения
-  }
-}
-
-class Magazine extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.type = 'magazine';
-  }
-}
-
-class Book extends PrintEditionItem {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.author = author;
-    this.type = 'book';
-  }
-}
-
-class NovelBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = 'novel';
-  }
-}
-
-class FantasticBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = 'fantastic';
-  }
-}
-
-class DetectiveBook extends Book {
-  constructor(author, name, releaseDate, pagesCount) {
-    super(author, name, releaseDate, pagesCount);
-    this.type = 'detective';
-  }
-}
-
-// Класс библиотеки
 class Library {
   constructor(name) {
     this.name = name;
@@ -177,7 +111,7 @@ class Library {
   }
 }
 
-// Тестовый сценарий
+// Пример использования
 const library = new Library("Библиотека имени Ленина");
 
 library.addBook(
